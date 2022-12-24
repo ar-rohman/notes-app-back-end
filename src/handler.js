24a170558e7af7a88db3fb4f2df8ad4b9dc91f3f
Rohman <rohmanpendo@gmail.com>
2022-12-24 19:58:40 +0700

    feat: notes list

--- src/handler.js
+++ src/handler.js
@@ -37,4 +37,9 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: { notes },
+});
+
+module.exports = { addNoteHandler, getAllNotesHandler };
